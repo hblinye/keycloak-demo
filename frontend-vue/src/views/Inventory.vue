afe7fb7755d210db44d16b81cd54b26da4b13740
hblinye <a1851yl@aiit.ac.jp>
2019-07-22 00:08:56 +0900

    restful pages

---
+++ frontend-vue/src/views/Inventory.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h3> Inventory Page </h3>
+    <router-link :to="{name: 'inventory_index'}">Index</router-link> | 
+    <router-link :to="{name: 'inventory_new'}">New</router-link>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
---
+++ frontend-vue/src/views/inventory/New.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h5> New Inventory </h5>
+    <label>
+      name:
+      <input type="text" v-model="inventory.name"/>
+    </label>
+    <button @click="save(inventory)">Submit</button>
+    <button @click="inventory.reset()">Reset</button>
+  </div>
+</template>
+
+<script>
+import { Inventory } from '../../models/inventory'
+import { mapActions } from 'vuex'
+export default {
+  data () {
+    return {
+      inventory: new Inventory()
+    }
+  },
+  methods: {
+    ...mapActions('inventory', ['load', 'save', 'destroy'])
+  }
+}
+</script>
+
